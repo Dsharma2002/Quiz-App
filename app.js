262fb166b721074d4ffd58f5d6dd58b0caf6c3e3
Divyansh Sharma <85435719+Dsharma2002@users.noreply.github.com>
2022-07-04 23:56:14 -0700

    Added question array and next button feature

--- app.js
+++ app.js
@@ -22,3 +22,78 @@ const app = () => {
 }
 
 app()
+
+// alert("Click Show Next to see the first question")
+
+const container = document.querySelector("#container");
+// check that the container is righfully selected
+// console.log(container);
+
+// declare an array that will host all the questions and related answers
+// can add more questions here 
+let array = [
+    {
+        question: "What is the preferred way for adding a background color in HTML?",
+        ansA: `<body background="yellow">`,
+        ansB: `<background>yellow</background>`,
+        ansC: `< body style="background-color:yellow">`,
+        ansD: `<background color="yellow">text<background>`
+    }
+];
+
+// counter helps with the rendering of suitable question accessed from array
+let counter = 0;
+let firstTime = true;
+const button = document.querySelector(".show-next");
+button.addEventListener("click", (e) => {
+    // check that the button is rightfully connected with the action
+    // console.log("Button was hit");
+    addQuestions();
+})
+
+function addQuestions() {
+    if (counter < array.length) {
+        // if condition to check that we never access array elements outside of array range 
+        if (container.hasChildNodes()) {
+            // if we already have a question displayed, remove this question
+            container.removeChild(container.lastChild)
+        }
+        // dynamically create a new div and add question in it
+        const newElement = document.createElement("div");
+        newElement.classList.add("supplied-question");
+        const newHeader = document.createElement("div")
+        newHeader.innerText = array[counter].question;
+        newElement.append(newHeader);
+        container.append(newElement);
+        // dynamically create a radio button for answer option A
+        const ansAA = document.createElement("label");
+        ansAA.innerText = array[counter].ansA;
+        const ansAR = document.createElement("input");
+        ansAR.type = "radio";
+        newElement.append(ansAR);
+        newElement.append(ansAA);
+        // dynamically create a radio button for answer option B
+        const ansBB = document.createElement("label");
+        ansBB.innerText = array[counter].ansB;
+        const ansBR = document.createElement("input");
+        ansBR.type = "radio";
+        newElement.append(ansBR);
+        newElement.append(ansBB);
+        // dynamically create a radio button for answer option B
+        const ansCC = document.createElement("label");
+        ansCC.innerText = array[counter].ansC;
+        const ansCR = document.createElement("input");
+        ansCR.type = "radio";
+        newElement.append(ansCR);
+        newElement.append(ansCC);
+        // dynamically create a radio button for answer option B
+        const ansDD = document.createElement("label");
+        ansDD.innerText = array[counter].ansD;
+        const ansDR = document.createElement("input");
+        ansDR.type = "radio";
+        newElement.append(ansDR);
+        newElement.append(ansDD);
+        // increment counter 
+        counter++;
+    }
+}
